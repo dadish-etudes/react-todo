f38757ba71811bcae56375b7b58a3ade82bbea1a
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2016-09-15 16:49:05 +0600

    Implement the delete completed tasks.

--- src/components/TodoFooter/TodoFooter.js
+++ src/components/TodoFooter/TodoFooter.js
@@ -6,21 +6,23 @@ import React, { Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './TodoFooter.scss';
 import classNames from 'classnames';
+import TodoActions from '../../actions/TodoActions';
 
 class TodoFooter extends Component {
 
 	constructor (props) {
 		super(props);
-		this.completedItems = this.completedItems.bind(this);
-		this.leftItemsCount = this.leftItemsCount.bind(this);
+		this.completeTasks = this.completeTasks.bind(this);
+		this.incompleteTaskCount = this.incompleteTaskCount.bind(this);
+		this.deleteCompleteTasks = this.deleteCompleteTasks.bind(this);
 	}
 
 	render () {
-		let leftItemsCount = this.leftItemsCount();
+		let incompleteTaskCount = this.incompleteTaskCount();
 		return(
 			<section className={this.props.allTodos.length ? s.root : s.hidden}>
 				<div className={s.left}>
-					{leftItemsCount} item{leftItemsCount === 1 ? '' : 's'} left
+					{incompleteTaskCount} item{incompleteTaskCount === 1 ? '' : 's'} left
 				</div>
 				
 				<div className={s.filter}>
@@ -29,19 +31,23 @@ class TodoFooter extends Component {
 					<div className={s.filterItem}>Completed</div>
 				</div>
 				
-				<div className={s.delete}>
+				<div className={s.delete} onClick={this.deleteCompleteTasks}>
 					Clear completed
 				</div>
 			</section>
 		);
 	}
 
-	completedItems () {
+	deleteCompleteTasks () {
+		this.completeTasks().forEach(item => TodoActions.destroy(item.id));
+	}
+
+	completeTasks () {
 		return this.props.allTodos.filter(item => item.complete);
 	}
 
-	leftItemsCount () {
-		return this.props.allTodos.length - this.completedItems().length;
+	incompleteTaskCount () {
+		return this.props.allTodos.length - this.completeTasks().length;
 	}	
 
 }
